c48bd08797b3d87869d54f8969a29f10c7461670
Cristiano Ramos <limaramos.c@gmail.com>
2022-08-29 15:47:26 -0300

    Projeto criado

---
+++ src/main/java/com/cristianoramos/serasaexperian/DesafioserasaexperianApplication.java
@@ -0,0 +1,13 @@
+package com.cristianoramos.serasaexperian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioserasaexperianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioserasaexperianApplication.class, args);
+	}
+
+}
